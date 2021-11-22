69c3b37ec21728769298feae9773587e264f1ca6
City93 <citypowah93@gmail.com>
2021-11-22 22:53:50 +0100

    Routes and controllers

---
+++ controllers/index.js
@@ -0,0 +1,31 @@
+const landing = (req, res) => {
+    res.render('index', { title: 'Express' });
+  }
+const signup = (req, res) => {
+    res.render('signup', { title: 'Express' });
+}
+const login = (req, res) => {
+    res.render('login', { title: 'Express' });
+}
+const favorites = (req, res) => {
+    res.render('favorites', { title: 'Express' });
+}
+const profile = (req, res) => {
+    res.render('profile', { title: 'Express' });
+}
+const users = (req, res) => {
+    res.render('users', { title: 'Express' });
+}
+const dashboard = (req, res) => {
+    res.render('dashboard', { title: 'Express' });
+}
+
+module.exports  = {
+    landing,
+    signup,
+    login,
+    favorites,
+    profile,
+    users,
+    dashboard
+}
