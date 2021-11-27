8cf1f8ec897f4ef2ce286ec8e6143e0709a6a2f1
fandibreak <jalid.acdhi@gmail.com>
2021-11-27 21:01:51 +0100

    Crud de mongo/models + i/bodyParser/express

--- models/crudMG.js
+++ models/crudMG.js
@@ -1,32 +1,38 @@
 const mongoose = require('../utils/mgBBDD')
+const body = require('body-parser')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
 //AQUI REALIZARE LOS CRUD INICIALES DE MONGO 
 //const url = "mongodb+srv://rooot:root@scrapapp.sloal.mongodb.net/ScrapColection";
 
 const objectSchema = {
     _id: mongoose.Schema.Types.ObjectId,
-    name: {
         name: String,
         author: String,
         author2: String,
-        duration: String
-    }, 
-    created: Date, 
+        duration: String,
+        created: Date
 
 };
-const userSchema = mongoose.Schema(objectSchema);
 
-let User = mongoose.model('User', userSchema);
+// Crear el esquema
+const ScrapSchemaMG = mongoose.Schema(objectSchema);
+
+const User = mongoose.model('User', ScrapSchemaMG);
+
+//HAGO EL CREATE 
+
 let scrap1 = {
     _id: new mongoose.Types.ObjectId(),
-    name: {
-        name: "Full Stack con Alejandro",
-        author: "Alejandro",
-        author2: "Reyes",
-        duration: "3 M"
-    }, 
-    created: Date.now()
+        name: "Angular + HTML",
+        author: "jalid",
+        author2: "Acdhi",
+        duration: "3 M",
+        created: Date.now()
 };
+
 let insertScrap = new User (scrap1);
 
 insertScrap.save(function(err){
@@ -34,3 +40,37 @@ insertScrap.save(function(err){
     console.log("Inserción correcta");
     mongoose.disconnect();
 });
+
+
+
+//HAGO GET CURSO Y LE APLICO FUNCION PARA QUE LE DE IGUAL M/m Y BUSQUE POR PALABRAS DENTRO DEL CURSO
+const getcurselike  = async (a) => {
+    console.log("*******************");
+    console.log(a);
+    // Consulta 
+    // Los datos
+    // del producto correspondiente
+    let data;
+    try{
+        if(1==1){
+            data = await User.find(({name: { $regex: '.*' + a + '.*',$options:'i' } }), '-__v');
+            
+            console.log(data)
+        }
+    }catch(err){
+        console.log(err)
+    }finally{
+
+        mongoose.disconnect();
+        console.log("conexion Cerrada")
+    }
+}
+
+//Prueba de llamada a cursos que contienen HTML
+
+getcurselike('CSS').then(data=>console.log(data))
+
+//Exporto el modulo para utilizar en otros scripts.
+
+module.exports = User;
+
