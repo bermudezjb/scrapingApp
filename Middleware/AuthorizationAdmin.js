41794d003c719241f450cdc9da0725a461a49037
fandibreak <jalid.acdhi@gmail.com>
2021-12-04 14:24:15 +0100

    Haseado de Psw en SngUp + comparacion de psw haseadas en login contra la BBDD

--- Middleware/AuthorizationAdmin.js
+++ Middleware/AuthorizationAdmin.js
@@ -4,22 +4,19 @@ function checkAdmin (token) {
   var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
-
   return JSON.parse(jsonPayload).role;
 
 };
 
 const authorizationAdmin = (req, res,next) => {
-  const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvZWwiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2Mzg0NDk2MzYsImV4cCI6MTYzODQ1MDgzNn0.ndH2EVww2kkNdyPf4zcEebaAa7FqWqCCvx0BqWdZNsU"
-    //const token = req.cookies.access_token;
+  //const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvZWwiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2Mzg0NDk2MzYsImV4cCI6MTYzODQ1MDgzNn0.ndH2EVww2kkNdyPf4zcEebaAa7FqWqCCvx0BqWdZNsU"
+    const token = req.cookies.access_token;
     if (checkAdmin(token)=="Admin") {
       next()
     } else {
       
       return res.sendStatus(403);
     }
-
-
   };
   
  // authorization()
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let controller = require('../controllers/index')
 //let users = require('../controllers/auth')
+const bcryptjs= require('bcryptjs')
 let jwt = require('jsonwebtoken')
 const accessTokenSecret = 'somerandomaccesstoken';
 const refreshTokenSecret = 'somerandomstringforrefreshtoken';
@@ -34,20 +35,18 @@ module.exports = router;
 
 router.post('/signup',controllerSql.dataentry); 
 
-
 router.post('/login', async (req, res) => {
 
     // read username and password from request body
-    const { username, password } = req.body; 
-    console.log({ username, password })
+    const {username,password} = req.body
 
     const users = await crudSql.getAllUserSistem()
-
     const usersAdmin = await crudSql.getATrueAdmin()
 
     // filter user from the users array by username and password
-    const user = users.find(u => { return u.username === username && u.password === password });
-    const userAdmin = usersAdmin.find(u => { return u.username === username && u.password === password });
+    //Con bcryptjs.compareSync comparo la pass actual de la BBDD(haseadas) vs las pass que le paso por el req.body deshaseada
+    const user = users.find(u => { return u.username === username && bcryptjs.compareSync(password,u.password)===true });
+    const userAdmin = usersAdmin.find( u => { return u.username === username &&  bcryptjs.compareSync(password,u.password)===true });
 
     if (user) {
         // generate an access token
@@ -55,13 +54,14 @@ router.post('/login', async (req, res) => {
         const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
 
         refreshTokens.push(refreshToken);
-        return res
-      .cookie("access_token", accessToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-    })
-    .status(200)
-    .json({ message: "Logged in successfully User 😊 👌" });
+        return res  
+                .cookie("access_token", accessToken, 
+                {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                })
+                .status(200)
+                .json({ message: "Logged in successfully User 😊 👌" });
 
     } else  if (userAdmin) {
         console.log('Es Admin')
@@ -71,21 +71,20 @@ router.post('/login', async (req, res) => {
 
         refreshTokens.push(refreshToken);
         return res
-      .cookie("access_token", accessToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-    })
-    .status(200)
-    .json({ message: "Logged in successfully ADMIN 😊 👌" });
+                .cookie("access_token", accessToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                })
+                .status(200)
+                .json({ message: "Logged in successfully ADMIN 😊 👌" });
 
     }else {
         res.send('Username or password incorrect ADMIN');
-        
     }
-        res.json({
-            accessToken,
-            refreshToken
-        });
+        // res.json({
+        //     accessToken,
+        //     refreshTokens
+        // });
 });
 
 
